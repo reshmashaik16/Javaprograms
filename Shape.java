2d200452f1c1d2d64cb04e95c5edda15280e5039
reshmashaik16 <70516215+reshmashaik16@users.noreply.github.com>
2020-10-22 20:43:24 +0530

    Add files via upload

--- Shape.java
+++ Shape.java
@@ -1,46 +1,29 @@
-public class Shape {
-	int n1;
-	int n2;
-	
-	void printArea() {
-		// Empty method
-	}
-	
-	// Driver Code for testing
-	public static void main(String[] args) {
-		Rectangle rt = new Rectangle();
-		rt.n1 = 2;
-		rt.n2 = 3;
-		rt.printArea();
-		
-		Triangle tr = new Triangle();
-		tr.n1 = 2;
-		tr.n2 = 3;
-		tr.printArea();
-		
-		Circle c = new Circle();
-		c.n1 = 2;
-		c.printArea();
-	}
-}
+abstract class Shape {
+	 int n1=4;
+	 int n2=6;
+	 abstract void printArea();
+	 public static void main(String[]args) {
+		 Shape s1=new Rectangle();
+		 s1.printArea();
+		 Shape s2=new Triangle();
+		 s2.printArea();
+		 Shape s3=new Circle();
+		 s3.printArea();
+	 }
 
-class Rectangle extends Shape {
-	void printArea() {
-		// In this case n1 = width, n2 = Length
-		System.out.println("Area of Rectangle: " + n1 * n2);
+}
+ class Rectangle extends Shape{
+	public void printArea() {
+		System.out.println("Rectangle area is "+n1*n2);
 	}
 }
-
-class Triangle extends Shape {
-	void printArea() {
-		// In this case n1 = Height, n2 = Base
-		System.out.println("Area of Rectangle: " + n1 * (n2 / 2));
+ class Triangle extends Shape{
+		public void printArea() {
+			System.out.println("triangle area is"+(n1*n2)/2);
+		}
 	}
-}
-
-class Circle extends Shape {
-	void printArea() {
-		// In this case n1 = radius, n2 = NULL
-		System.out.println("Area of Rectangle: " + Math.PI * n1 * n1);
+ class Circle extends Shape{
+		public void printArea() {
+			System.out.println("circle area is "+(Math.PI*n1*n1));
+		}
 	}
-}
